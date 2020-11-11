a2ae72382a9ee81324ebb1de86fb4ec875628d99
Hannah Malm <hannahemalm616@gmail.com>
2020-11-11 00:50:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,11 +4,13 @@ def oxford_comma(string)
 #when the string size is 0, return ""  
   when 0
     ""
-#when the string size is 1, return string.first    
+#when the string size is 1, return string.first (a 1-element array) without any formatting  
   when 1
     string.first
+#when the string is a 2 element, array return the join with ' and ' in between elements    
   when 2
     string.join(' and ')
+#when array length is greater than three, return the formatting and join And    
   else
     [string[0..-2].join(', '), string.last].join(', and ')
   end
