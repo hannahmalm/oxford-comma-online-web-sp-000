14e09b69f4e4a9c8453a99ef5a6b11f7297f051b
Hannah Malm <hannahemalm616@gmail.com>
2020-11-11 00:46:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,10 @@
 def oxford_comma(string)
 #takes a case statement definition of string size  
   case string.size
+#when the string size is 0, return ""  
   when 0
     ""
+#when the string size is 1, return string.first    
   when 1
     string.first
   when 2
